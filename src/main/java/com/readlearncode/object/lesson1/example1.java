98bb20c3a541f807216f96103e3dd533f73c9d01
Alex Theedom <alex.theedom@gmail.com>
2017-03-16 17:45:34 +0100

    Add project structure

---
+++ src/main/java/com/readlearncode/object/lesson1/example1.java
@@ -0,0 +1,17 @@
+package com.readlearncode.object.lesson1;
+
+/**
+ * Source code github.com/readlearncode
+ *
+ * Creating an object model from JSON Data
+ *
+ * @author Alex Theedom www.readlearncode.com
+ * @version 1.0
+ */
+public class example1 {
+
+
+
+
+
+}
