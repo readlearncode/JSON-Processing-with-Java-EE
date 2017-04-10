1fd89163b2eb5d1da93c3583979ab2293bcf80f3
Alex Theedom <alex.theedom@gmail.com>
2017-04-10 07:01:48 -0700

    Organise classes for lesson 5 start state

---
+++ streaming-lesson-5-start/src/main/java/com/readlearncode/streaming/lesson5/StreamingExample3.java
@@ -0,0 +1,23 @@
+package com.readlearncode.streaming.lesson5;
+
+import javax.json.Json;
+import javax.json.JsonValue;
+import javax.json.stream.JsonGenerator;
+import java.io.StringWriter;
+
+/**
+ * Source code github.com/readlearncode
+ *
+ * @author Alex Theedom www.readlearncode.com
+ * @version 1.0
+ */
+public class StreamingExample3 {
+
+    public String writeJsonStreamToString() {
+
+        // Implement logic that constructs a JSON object using the Streaming model
+
+        return null;
+    }
+
+}
---
+++ streaming-lesson-5-start/src/main/java/com/readlearncode/streaming/lesson5/StreamingExample4.java
@@ -0,0 +1,25 @@
+package com.readlearncode.streaming.lesson5;
+
+import javax.json.Json;
+import javax.json.JsonValue;
+import javax.json.stream.JsonGenerator;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Source code github.com/readlearncode
+ *
+ * @author Alex Theedom www.readlearncode.com
+ * @version 1.0
+ */
+public class StreamingExample4 {
+
+    public static void main(String... args) throws IOException {
+
+        // Implement logic that write JSON data to a flat-file
+
+    }
+}
+
+
