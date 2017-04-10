6a8acaeee5d59f6eeaa8d8ab474b3aba688c4cd3
Alex Theedom <alex.theedom@gmail.com>
2017-04-09 22:09:47 -0700

    Organise classes for lesson 5 start state

---
+++ object-lesson-5-start/src/test/java/com/readlearncode/object/lesson5/ObjectExample6Test.java
@@ -0,0 +1,31 @@
+package com.readlearncode.object.lesson5;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+/**
+ * Source code github.com/readlearncode
+ *
+ * @author Alex Theedom www.readlearncode.com
+ * @version 1.0
+ */
+public class ObjectExample6Test {
+
+    private static String json = "{\"title\":\"JSON-Processing With Java EE\",\"chapters\":[\"Introduction\",\"1. JSON and Java\",\"2. JSON-Processing API features\",\"3. The Java EE JSON Object model\",\"4. The Java EE JSON Streaming model\",\"Conclusion\"],\"released\":true,\"length\":90,\"sourceCode\":{\"repositoryName\":\"JSON-Processing-with-Java-EE\",\"url\":\"github.com/readlearncode\"},\"complementaryCourse\":[{\"title\":\"RESTful Service with JAX-RS 2.0\",\"length\":120},{\"title\":\"Java Enterprise Edition Introduction\",\"length\":130}],\"notes\":null}";
+
+    @Test
+    public void givenJsonObject_shouldWriteCorrectlyToString() throws Exception {
+
+        // arrange
+        ObjectExample6 objectExample6 = new ObjectExample6();
+
+        // act
+        String jsonString = objectExample6.writeJsonStructure();
+
+        // assert
+        // Assert that the JSON outputted is equal to the JSON string
+
+    }
+
+}
