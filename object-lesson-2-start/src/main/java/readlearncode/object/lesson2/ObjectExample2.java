74c2a53a343ed45d13e432f5de118e21a8b55ca1
Alex Theedom <alex.theedom@gmail.com>
2017-04-09 19:27:36 -0700

    Orgnise classes for start state of lesson 2

---
+++ object-lesson-2-start/src/main/java/readlearncode/object/lesson2/ObjectExample2.java
@@ -0,0 +1,51 @@
+package readlearncode.object.lesson2;
+
+import javax.json.*;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import static readlearncode.Utils.ROOT;
+
+/**
+ * Source code github.com/readlearncode
+ * <p>
+ * Creating an object model from JSON Data
+ *
+ * @author Alex Theedom www.readlearncode.com
+ * @version 1.0
+ */
+public class ObjectExample2 {
+
+
+    /**
+     * Builds a JsonObject from a flat-file source containing Json data.
+     *
+     * @return a JsonObject built from the flat-file
+     * @throws FileNotFoundException
+     */
+    public JsonObject loadJsonObject() throws FileNotFoundException {
+
+        String jsonFile = ROOT + System.getProperty("file.separator") + "example1-jsondata-object.json";
+
+        // Implement code that loads the file and creates a JSON object from the files contents
+
+        return null;
+
+    }
+
+    /**
+     * Builds a JsonArray from a flat-file source containing Json Array data.
+     *
+     * @return a JsonArray built from the flat-file
+     * @throws FileNotFoundException
+     */
+    public JsonArray loadJsonArray() throws FileNotFoundException {
+
+        String jsonFile = ROOT + System.getProperty("file.separator") + "example1-jsondata-array.json";
+
+        // Implement code that loads JSON array data from a flat-file and creates a JsonArray object
+
+        return null;
+    }
+
+}
---
+++ object-lesson-2-start/src/test/java/readlearncode/object/lesson2/ObjectExample1Test.java
@@ -0,0 +1,33 @@
+package readlearncode.object.lesson2;
+
+import org.junit.Test;
+
+import javax.json.JsonObject;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+/**
+ * Source code github.com/readlearncode
+ *
+ * @author Alex Theedom www.readlearncode.com
+ * @version 1.0
+ */
+public class ObjectExample1Test {
+
+
+    @Test
+    public void givenJsonDataInString_readsDataIntoJsonObject() throws Exception {
+
+        // Arrange
+        ObjectExample1 objectExample1 = new ObjectExample1();
+
+        // Act
+        JsonObject jsonObject = objectExample1.loadJsonString();
+
+        // Assert
+        // Implement code that asserts each element of the JSON document has been
+        // appropriately interpreted and loaded into a JSON object.
+
+    }
+
+}
