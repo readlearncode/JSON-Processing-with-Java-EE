e3ed2c90b1be77a874d81e17cd5c7341f11d93ad
Alex Theedom <alex.theedom@gmail.com>
2017-04-09 19:28:05 -0700

    Organise classes for end state of lesson 2

---
+++ object-lesson-2-end/src/test/java/lesson2/ObjectExample2Test.java
@@ -0,0 +1,58 @@
+package lesson2;
+
+import org.junit.Test;
+import readlearncode.object.lesson2.ObjectExample2;
+
+import javax.json.*;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+/**
+ * Source code github.com/readlearncode
+ *
+ * @author Alex Theedom www.readlearncode.com
+ * @version 1.0
+ */
+public class ObjectExample2Test {
+
+    @Test
+    public void givenJsonDateInFlatFile_readsDataIntoJsonObject() throws Exception {
+
+        // Arrange
+        ObjectExample2 objectExample2 = new ObjectExample2();
+
+        // Act
+        JsonObject jsonObject = objectExample2.loadJsonObject();
+
+        // Assert
+        assertThat(jsonObject.getValueType()).isEqualTo(JsonValue.ValueType.OBJECT);
+    }
+
+    @Test
+    public void givenJsonArrayDataInFlatFile_readsDataIntoJsonArray() throws Exception {
+
+        // Arrange
+        ObjectExample2 objectExample2 = new ObjectExample2();
+
+        // Act
+        JsonArray jsonArray = objectExample2.loadJsonArray();
+
+        // Assert
+        assertThat(jsonArray.getValueType()).isEqualTo(JsonValue.ValueType.ARRAY);
+    }
+
+
+    @Test
+    public void givenJsonArrayDataInFlatFile_readsDataIntoJsonStructure() throws Exception {
+
+        // Arrange
+        ObjectExample2 objectExample2 = new ObjectExample2();
+
+        // Act
+        JsonStructure jsonStructure = objectExample2.loadJsonStructure();
+
+        // Assert
+        assertThat(jsonStructure.getValueType()).isEqualTo(JsonValue.ValueType.ARRAY);
+    }
+
+}
