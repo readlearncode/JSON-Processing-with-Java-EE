437e5649c51449894bbee621cde03b722458924e
Alex Theedom <readlearncode@gmail.com>
2017-04-20 16:53:10 -0700

    Fix end state and solutions

--- 04_02_end/src/main/java/com/readlearncode/streaming/lesson2/StreamingExample2.java
+++ 04_02_end/src/main/java/com/readlearncode/streaming/lesson2/StreamingExample2.java
@@ -1,12 +1,5 @@
 package com.readlearncode.streaming.lesson2;
 
-import javax.json.Json;
-import javax.json.stream.JsonParser;
-import java.io.StringReader;
-
-import static javax.json.stream.JsonParser.Event.VALUE_NUMBER;
-import static javax.json.stream.JsonParser.Event.VALUE_STRING;
-
 /**
  * Source code github.com/readlearncode
  *
@@ -18,21 +11,8 @@ public class StreamingExample2 {
 
     public String retrieveValue(final String key_to_find, final String json) {
 
-        JsonParser parser = Json.createParser(new StringReader(json));
-
-        while (parser.hasNext()) {
-            JsonParser.Event event = parser.next();
-            switch (event) {
-                case KEY_NAME:
-                    if (parser.getString().equalsIgnoreCase(key_to_find)) {
-                        event = parser.next();
-                        if (event == VALUE_STRING || event == VALUE_NUMBER) {
-                            return parser.getString();
-                        }
-                    }
-                    break;
-            }
-        }
+        // Implement logic that traverse a JSON document and retrieves
+        // the value of a given key.
 
         return null;
     }
